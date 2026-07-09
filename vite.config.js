207276adc617fb1ca87cf3bab22d298152c30e23
JYzfDmm <976449220@qq.com>
2026-07-09 22:18:39 +0800

    fix: 配置github pages基础路径

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/forest-fire-exam/', // 新增这一行，仓库名前后斜杠不能省略
   server: {
     host: '0.0.0.0',
     port: 5173
